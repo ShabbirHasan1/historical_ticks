5b66b0f50ecd20571d42aff8b0325d8f3e29f81e
ebtrader <jsiddique@gmail.com>
2021-07-26 13:55:16 -0400

    Commit

--- wheel/acct_value.py
+++ wheel/acct_value.py
@@ -1,14 +1,6 @@
 import logging
-
-import time
-
-
 import pandas as pd
-
 from ibapi.utils import iswrapper
-
-from ContractSamples import ContractSamples
-
 from ibapi.client import EClient
 from ibapi.wrapper import EWrapper
 # types
@@ -20,81 +12,18 @@ class TestApp(EWrapper, EClient):
         EWrapper.__init__(self)
         EClient.__init__(self, wrapper=self)
         # ! [socket_init]
-        self.nKeybInt = 0
-        self.started = False
-        self.nextValidOrderId = None
-        self.permId2ord = {}
         self.globalCancelOnly = False
         self.simplePlaceOid = None
-        self._my_errors = {}
         self.data = []  # Initialize variable to store candle
         self.df = pd.DataFrame()
 
-    @iswrapper
-    # ! [connectack]
-    def connectAck(self):
-        if self.asynchronous:
-            self.startApi()
-
-    # ! [connectack]
-
-    @iswrapper
-    # ! [nextvalidid]
     def nextValidId(self, orderId: int):
-        super().nextValidId(orderId)
-
-        logging.debug("setting nextValidOrderId: %d", orderId)
-        self.nextValidOrderId = orderId
-        print("NextValidId:", orderId)
-        # ! [nextvalidid]
-
         # we can start now
         self.start()
 
     def start(self):
-        if self.started:
-            return
-
-        self.started = True
-
-        if self.globalCancelOnly:
-            print("Executing GlobalCancel only")
-            self.reqGlobalCancel()
-        else:
-            print("Executing requests")
-            self.accountOperations_req()
-            print("Executing requests ... finished")
-
-    def keyboardInterrupt(self):
-        self.nKeybInt += 1
-        if self.nKeybInt == 1:
-            self.stop()
-        else:
-            print("Finishing test")
-            self.done = True
-
-    def stop(self):
-        print("Executing cancels")
-
-
-        print("Executing cancels ... finished")
-
-    def nextOrderId(self):
-        oid = self.nextValidOrderId
-        self.nextValidOrderId += 1
-        return oid
-
-    @iswrapper
-    # ! [error]
-    def error(self, reqId: TickerId, errorCode: int, errorString: str):
-        super().error(reqId, errorCode, errorString)
-        print("Error. Id:", reqId, "Code:", errorCode, "Msg:", errorString)
-        errormsg = "IB error id %d errorcode %d string %s" % (reqId, errorCode, errorString)
-        self._my_errors = errormsg
-
-    @iswrapper
-    def winError(self, text: str, lastError: int):
-        super().winError(text, lastError)
+        self.accountOperations_req()
+        print("Executing requests ... finished")
 
     def accountOperations_req(self):
         # Requesting accounts' summary
@@ -106,13 +35,13 @@ class TestApp(EWrapper, EClient):
     def accountSummary(self, reqId: int, account: str, tag: str, value: str,
                        currency: str):
         super().accountSummary(reqId, account, tag, value, currency)
-        print("AccountSummary. ReqId:", reqId, "Account:", account,
-              "Tag: ", tag, "Value:", value, "Currency:", currency)
+        # print("AccountSummary. ReqId:", reqId, "Account:", account,
+        #       "Tag: ", tag, "Value:", value, "Currency:", currency)
         self.data.append([tag, value])
         self.df = pd.DataFrame(self.data, columns=['Account', 'Value'])
-        print(self.df)
-        self.df.to_csv('acct_value.csv')
         if len(self.df) == 24:
+            print(self.df)
+            self.df.to_csv('acct_value.csv')
             net_liquid = self.df.loc[8,'Value']
             five_pct_rule = .05 * float(net_liquid)
             print(f'net liquidation value: {net_liquid}')
@@ -120,22 +49,10 @@ class TestApp(EWrapper, EClient):
         self.disconnect()
 
 def main():
-
     app = TestApp()
-    try:
-
-        # ! [connect]
-        app.connect("127.0.0.1", port=7497, clientId=102)
-        # ! [connect]
-        print("serverVersion:%s connectionTime:%s" % (app.serverVersion(),
-                                                      app.twsConnectionTime()))
-        # ! [clientrun]
-        app.run()
-
-
-        # ! [clientrun]
-    except:
-        raise
+    app.connect("127.0.0.1", port=7497, clientId=102)
+    print("serverVersion:%s connectionTime:%s" % (app.serverVersion(),app.twsConnectionTime()))
+    app.run()
 
 if __name__ == "__main__":
     main()
